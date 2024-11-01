3e6012ba895783dafe75d5d52971fb9ee2ecae8c
Austin K <austink@Austins-MacBook-Pro.local>
2024-11-01 00:07:58 -0400

    REVERSE TDD ACTIVATE

--- spec/requests/subscriptions/subscription_create_spec.rb
+++ spec/requests/subscriptions/subscription_create_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "Subscription Creation" do
   before(:each) do
     @customer = Customer.create!(first_name: "John", last_name: "Wick", email: "tea@gmail.com", address: "123 tea ave")
     @tea = Tea.create!(tea_name: "Green Tea", description: "Lipton", temperature: 80, brew_time: 3)
+    @tea2 = Tea.create!(tea_name: "Oolong", description: "Japan's Finest", temperature: 68, brew_time: 5)
     @subscription = @customer.subscriptions.create!(tea: @tea, sub_name: "Tea Boxes", price: 9.99, status: "active", frequency: "monthly")
+    @subscription2 = @customer.subscriptions.create!(tea: @tea2, sub_name: "Tea Bags", price: 5.99, status: "disabled", frequency: "yearly")
   end
   it "creates a new subscription" do
 
@@ -24,5 +26,17 @@ RSpec.describe "Subscription Creation" do
     expect(response).to have_http_status(200)
     json = JSON.parse(response.body, symbolize_names: true)
     expect(json[:status]).to eq("disabled")
+  end
+
+  it " should show all subscriptions for a customer" do
+    get "/api/v1/customers/#{@customer.id}/subscriptions"
+
+    expect(response).to have_http_status(200)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to be_an(Array)
+    expect(json.size).to eq(2)
+    expect(json.first[:sub_name]).to eq("Tea Boxes")
+    expect(json.second[:sub_name]).to eq("Tea Bags")
   end
 end
