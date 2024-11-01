4e7ee9fcaad0339f18d2e0bc798fec64d0cbe293
Austin K <austink@Austins-MacBook-Pro.local>
2024-11-01 00:14:05 -0400

    Added seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+    customer = Customer.create!(first_name: "John", last_name: "Wick", email: "tea@gmail.com", address: "123 tea ave")
+    tea = Tea.create!(tea_name: "Green Tea", description: "Lipton", temperature: 80, brew_time: 3)
+    tea2 = Tea.create!(tea_name: "Oolong", description: "Japan's Finest", temperature: 68, brew_time: 5)
+    subscription = customer.subscriptions.create!(tea: tea, sub_name: "Tea Boxes", price: 9.99, status: "active", frequency: "monthly")
+    subscription2 = customer.subscriptions.create!(tea: tea2, sub_name: "Tea Bags", price: 5.99, status: "disabled", frequency: "yearly")
